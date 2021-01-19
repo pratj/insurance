bf35fdd891b8b78a9f03ebb4e4a1d40db0ad19e9
pratj <prateek.jo97@gmail.com>
2021-01-19 10:38:28 +0530

    made changes to charge

---
+++ insurance-main/backend/src/main/kotlin/com/manipal/insurance/InsuranceApplication.kt
@@ -0,0 +1,13 @@
+package com.manipal.insurance
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import springfox.documentation.swagger2.annotations.EnableSwagger2
+
+@SpringBootApplication
+@EnableSwagger2
+class InsuranceApplication
+
+fun main(args: Array<String>) {
+    runApplication<InsuranceApplication>(*args)
+}
